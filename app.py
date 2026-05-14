df0a849a42efbf1405023d1e9a297be8b11c0b48
Jason Adrian Nuñez <jasonadrian.zenun@gmail.com>
2026-05-14 14:51:13 +0800

    added images of cat,dog and random and code for gradio and ml

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper() 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+learn = load_learner('model.pkl')
+
+
+categories = ('Dog', 'Cat')
+
+def classify_img(img):
+  pred,idx,probs = learn.predict(img)
+  return dict(zip(categories, map(float, probs)))
+
+
+image = gr.inputs.Image(shape =(192, 192))
+label = gr.outputs.Label()
+examples = ['dog.jfif', 'cat.jpg', 'random.jpg']
+
+intf = gr.Interface(fn=classify_img, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
+
+ 
